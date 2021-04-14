ad24f8d816f4af52db8ba7fa4c31f9a52d289708
Pec Muhn <79809244+wnaBee@users.noreply.github.com>
2021-04-14 18:27:13 +0200

    Add files via upload

--- Sensor_Read_Arduino/Sensor_Read_Arduino.ino
+++ Sensor_Read_Arduino/Sensor_Read_Arduino.ino
@@ -4,21 +4,26 @@
 #define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
-float reqhum = 0;
+float reqAhum = 0;
 float reqtemp = 0;
+float reqShum = 0;
 float ActiveTime = 0;
 float InActiveTime = 0;
+String locale[] = {"meny","temperature","Soil humidity","Air humidity","Active time","Inactive time"};
+int clocale = 0;
+int Mpos = 0;
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
-}
-
-void loop() {
-  while( reqhum == 0 || reqtemp == 0 || ActiveTime == 0 || InActiveTime == 0){
-    if(reqhum == 0){
+  Serial.print("Input in order, deliniate with \\n: top Air humidity, top Soil humidity, minimum temperature \nactive time, inactive time \n");
+  while( reqAhum == 0 || reqShum == 0 || reqtemp == 0 || ActiveTime == 0 || InActiveTime == 0){
+    if(reqAhum == 0){
       while(Serial.available() == 0){}
-      reqhum = Serial.parseFloat();
+      reqAhum = Serial.parseFloat();
+    }else if(reqShum == 0){
+      while(Serial.available() == 0){}
+      reqShum = Serial.parseFloat();
     }else if(reqtemp == 0){
       while(Serial.available() == 0){}
       reqtemp = Serial.parseFloat();
@@ -31,19 +36,17 @@ void loop() {
     }
   }
   //confirm user choices
-  Serial.print("Required Humidity: ");
-  Serial.print(reqhum);
-  Serial.print("%"); 
-  Serial.print("  |  ");
-  Serial.print("Required Temperature: ");
+  Serial.print("Required Air Humidity: ");
+  Serial.print(reqAhum); 
+  Serial.print("%  |  Required Soil Humidity: ");
+  Serial.print(reqShum);
+  Serial.print("%  |  Required Temperature: ");
   Serial.print(reqtemp);
   Serial.print(" °C");
 
   Serial.print("\nSpraying time: ");
-  Serial.print(ActiveTime/1000);
-  Serial.print(" s"); 
-  Serial.print("  |  ");
-  Serial.print("Spraying Pause: ");
+  Serial.print(ActiveTime/1000); 
+  Serial.print(" s  |  Spraying Pause: ");
   Serial.print(InActiveTime/1000); 
   Serial.print(" s");
   
@@ -54,18 +57,139 @@ void loop() {
     delay(1000);
     }
   Serial.print("\n");
-  
+}
+
+int menu(){
+    //down
+    if(digitalRead(4) == HIGH){
+      //Serial.print(digitalRead(4));
+      switch(clocale){
+        case 0:
+          //menu
+          Mpos--;
+          if(Mpos < 0){Mpos = 5;}
+          Serial.print(locale[Mpos]);
+          break;
+        case 1:
+          //temperature
+          reqtemp--;
+          Serial.print(reqtemp);
+          break;
+        case 2:
+          //soil humidity
+          reqShum--;
+          Serial.print(reqShum);
+          break;
+        case 3:
+          //Air humidity
+          reqAhum--;
+          Serial.print(reqAhum);
+          break;
+        case 4:
+          //active time
+          ActiveTime -= 1000;
+          Serial.print(ActiveTime/1000);
+          break;
+        case 5:
+          //inactive time
+          InActiveTime -= 1000;
+          Serial.print(InActiveTime/1000);
+          break;
+        }
+        Serial.print("\n");
+        delay(1000);
+    }
+    //confirm
+    if(digitalRead(5) == HIGH){
+      Serial.print("entered\n");
+      //Serial.print(digitalRead(5));
+      switch(clocale){
+        case 0:
+          //menu
+          clocale = Mpos;
+          break;
+        case 1:
+          //temperature
+          clocale = 0;
+          break;
+        case 2:
+          //Soil humidity
+          clocale = 0;
+          break;
+        case 3:
+          //Air humidity
+          clocale = 0;
+          break;
+        case 4:
+          //Active time
+          clocale = 0;
+          break;
+        case 5:
+          //Inactive time
+          clocale = 0;
+          break;
+        }
+        Serial.print(locale[Mpos]);
+        Serial.print("\n");
+        delay(1000);
+      }
+    //up
+    if(digitalRead(6) == HIGH){
+      //Serial.print(digitalRead(6));
+      switch(clocale){
+        case 0:
+          //menu
+          Mpos++;
+          if(Mpos > 5){Mpos = 0;}
+          Serial.print(locale[Mpos]);
+          break;
+        case 1:
+          //temperature
+          reqtemp++;
+          Serial.print(reqtemp);
+          break;
+        case 2:
+          //soil humidity
+          reqShum++;
+          Serial.print(reqShum);
+          break;
+        case 3:
+          //air humidity
+          reqAhum++;
+          Serial.print(reqAhum);
+          break;
+        case 4:
+          //active time
+          ActiveTime += 1000;
+          Serial.print(ActiveTime/1000);
+          break;
+        case 5:
+          //inactive time
+          InActiveTime += 1000;
+          Serial.print(InActiveTime/1000);
+          break;
+        }
+        Serial.print("\n");
+        delay(1000);
+    }
+    return 0;
+  }
+
+void loop() {
   //main body
-  while(1==1){
+    menu();
     float hum = dht.readHumidity();
+    float Shum = dht.readHumidity();
     float temp = dht.readTemperature();
+    
     
     //verify integrity
     if (isnan(hum) || isnan(temp)) {
-      Serial.println("Failed to read from DHT sensor!");
+      Serial.println("Failed to read from DHT sensor");
     } else {
     
       //print situation
+/*
       Serial.print("Humidity: ");
       Serial.print(hum);
       Serial.print("%");
@@ -75,16 +199,15 @@ void loop() {
       Serial.print("Temperature: ");
       Serial.print(temp);
       Serial.print("°C\n");
-
+*/
       //water
-      if(temp > reqtemp || hum < reqhum){
+      if(temp > reqtemp || hum < reqAhum || Shum < reqShum){
         Serial.print("watering\n");
         float initTime = millis();
-        while(millis()-initTime < ActiveTime){}
+        while(millis()-initTime < ActiveTime){menu();}
         Serial.print("done\n");
         initTime = millis();
-        while(millis()-initTime < InActiveTime){}
+        while(millis()-initTime < InActiveTime){menu();}
       }
-    }
-  }
+   }
 }
